f080d952d835ae90936da9789b7eb59c1fb37a7e
bnonni <bryanwnonni@gmail.com>
2020-07-09 16:12:55 -0400

    Fix edge cases

--- findStray.js
+++ findStray.js
@@ -2,7 +2,11 @@ function stray(numbers) {
  var strayN = numbers[0]
  for(var i = 0; i <= numbers.length; i++){
   var temp = numbers[i+1];
-  if(temp != strayN){
+  
+  if(numbers[0] != numbers[1]){
+   strayN = numbers[0];
+   break;
+  } else if(temp != strayN){
    strayN = temp;
    break;
   }
@@ -12,3 +16,5 @@ function stray(numbers) {
 
 console.log(stray([1, 1, 2]))
 console.log(stray([17, 17, 3, 17, 17, 17, 17]))
+console.log(stray([8, 1, 1, 1, 1, 1, 1, 1]))
+console.log(stray([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9]))
